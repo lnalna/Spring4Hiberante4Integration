f63cee4b809ed1c3025fe30f6edcf46f08db32d4
Nikolay <lnalna@yandex.ru>
2016-06-21 15:42:14 +0300

    change EmployeeDAO

---
+++ src/main/java/com/ranga/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package com.ranga.dao;
+
+import java.util.List;
+
+import com.ranga.entity.Employee;
+
+
+
+public interface EmployeeDAO {
+	public long createEmployee(Employee employee);
+    public Employee updateEmployee(Employee employee);
+    public void deleteEmployee(long id);
+    public List<Employee> getAllEmployees();
+    public Employee getEmployee(long id);	
+	public List<Employee> getAllEmployees(String employeeName);
+}
