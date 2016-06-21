bd9c503ce9ec7515cc8a50735687a35e6e027aaa
Nikolay <lnalna@yandex.ru>
2016-06-21 16:01:38 +0300

    // import java.util.List

--- src/main/java/com/ranga/controller/EmployeeController.java
+++ src/main/java/com/ranga/controller/EmployeeController.java
@@ -7,7 +7,7 @@ import org.jboss.logging.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+//import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
